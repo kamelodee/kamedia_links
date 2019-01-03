b0f648698ab4cccce70abe042038c99c5cb9af88
kamelodee <39636802+kamelodee@users.noreply.github.com>
2019-01-03 10:36:43 +0000

    Add files via upload

---
+++ green/kam.js
@@ -0,0 +1,12 @@
+//alert("am here")
+//var x=4;
+//var y=7;
+//var z=x+y;
+
+//function alertmsg(){
+//alert("wecome to kamilo");
+//alert("goodbye kamilo");
+//}
+function MySysDate(){
+    document.getElementById("ik").innerHTML="my phone number is 0248907440";
+}
